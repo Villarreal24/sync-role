e60865e8a2a5b5afd07b249b36ad4f7af7f2c166
Angel Villarreal <30707320+Villarreal24@users.noreply.github.com>
2026-07-06 09:48:29 -0700

    Installed nitro package to fix ssr in production

--- sync-role/vite.config.ts
+++ sync-role/vite.config.ts
@@ -1,15 +1,22 @@
 import { defineConfig } from 'vite'
 import { devtools } from '@tanstack/devtools-vite'
-
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { nitro } from 'nitro/vite'
 
 const config = defineConfig({
   resolve: { tsconfigPaths: true },
   envPrefix: ['VITE_', 'BACKEND_'],
-  plugins: [devtools(), tailwindcss(), tanstackStart(), viteReact()],
+  plugins: [
+    devtools(),
+    tailwindcss(),
+    tanstackStart(),
+    viteReact(),
+    nitro({
+      preset: process.env.VERCEL ? 'vercel' : undefined,
+    }),
+  ],
 })
 
 export default config
